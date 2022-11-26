dede3340f51414060f452dd3e5902c5daedbde9d
nofamex <nsiren517@gmail.com>
2022-11-26 07:54:15 +0700

    prod ready

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack(config) {
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
