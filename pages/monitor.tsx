d96603177612a385896fe6fac535a9ef0d8ea2d0
nofamex <nsiren517@gmail.com>
2022-11-28 15:10:14 +0700

    fix monitor habe

--- pages/monitor.tsx
+++ pages/monitor.tsx
@@ -1,226 +1,95 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import busMonitor from "../public/assets/image/bus/busMonitor.svg";
-import sepi from "../public/assets/image/monitor/sepi.svg";
 import penuh from "../public/assets/image/monitor/penuh.svg";
+import MonitorTimer, {
+  MonitorEstimate,
+  MonitorRoute,
+  MonitorStatus,
+} from "../components/monitor/timer";
+
+const mockData = [
+  {
+    number: 1,
+    estimate: "15:30",
+    timeLeft: 10,
+    route: "LURUS",
+    status: "SEPI",
+  },
+  {
+    number: 2,
+    estimate: "15:37",
+    timeLeft: 17,
+    route: "LURUS",
+    status: "NORMAL",
+  },
+  {
+    number: 3,
+    estimate: "15:44",
+    timeLeft: 24,
+    route: "LURUS",
+    status: "PENUH",
+  },
+];
 
 export default function monitor() {
   const [i, setI] = useState(0);
 
+  const getInterval = (mode: number) => {
+    switch (mode) {
+      case 0:
+        return 15000;
+      case 1:
+        return 15000;
+      case 2:
+        return 5000;
+      default:
+        setI(0);
+        return 50000;
+    }
+  };
+
   useEffect(() => {
     const interval = setInterval(() => {
-      setI(prevCount => prevCount + 1);
-      if (i === 3) {
-        setI(0)
-      }
-    }, 15000);
+      setI((prevCount) => prevCount + 1);
+    }, getInterval(i));
     return () => clearInterval(interval);
-  })
+  });
 
   return (
     <>
       {i === 0 ? (
         <>
-          {" "}
           <div className="bg-black h-screen w-screen flex flex-col space-y-6 p-8 text-white">
-            <div className="h-1/3 border-2 rounded-xl flex flex-row justify-between items-center p-6">
-              <div className="w-1/5 flex space-x-4">
-                <div>
-                  <Image alt="" src={busMonitor} />
-                </div>
-                <div className="bg-white rounded-xl px-12 py-2 my-auto">
-                  <p className="text-black text-7xl">2</p>
-                </div>
-              </div>
-              <div className="w-4/5 flex justify-end">
-                <p className="text-white text-8xl">
-                  akan tiba - <span className="font-semibold"> 08:37</span>{" "}
-                </p>
-              </div>
-            </div>
-            <div className="h-1/3 border-2 rounded-xl flex flex-row justify-between items-center p-6">
-              <div className="w-1/5 flex space-x-4">
-                <div>
-                  <Image alt="" src={busMonitor} />
-                </div>
-                <div className="bg-white rounded-xl px-12 py-2 my-auto">
-                  <p className="text-black text-7xl">2</p>
-                </div>
-              </div>
-              <div className="w-4/5 flex justify-end">
-                <p className="text-white text-8xl">
-                  akan tiba - <span className="font-semibold"> 08:37</span>{" "}
-                </p>
-              </div>
-            </div>{" "}
-            <div className="h-1/3 border-2 rounded-xl flex flex-row justify-between items-center p-6">
-              <div className="w-1/5 flex space-x-4">
-                <div>
-                  <Image alt="" src={busMonitor} />
-                </div>
-                <div className="bg-white rounded-xl px-12 py-2 my-auto">
-                  <p className="text-black text-7xl">2</p>
-                </div>
-              </div>
-              <div className="w-4/5 flex justify-end">
-                <p className="text-white text-8xl">
-                  akan tiba - <span className="font-semibold"> 08:37</span>{" "}
-                </p>
-              </div>
-            </div>
-          </div>{" "}
+            {mockData.map((d) => {
+              return <MonitorTimer estimate={d.estimate} num={d.number} />;
+            })}
+          </div>
         </>
       ) : i === 1 ? (
         <>
-          {" "}
           <div className="bg-black h-screen w-screen flex flex-col space-y-6 p-8 text-white">
-            <div className="h-1/3 border-2 rounded-xl flex flex-row justify-between items-center p-6">
-              <div className="w-1/5 flex space-x-4">
-                <div>
-                  <Image alt="" src={busMonitor} />
-                </div>
-                <div className="bg-white rounded-xl px-12 py-2 my-auto">
-                  <p className="text-black text-7xl">2</p>
-                </div>
-              </div>
-              <div className="w-4/5 flex justify-end">
-                <p className="text-white text-8xl font-semibold">10 Menit </p>
-              </div>
-            </div>{" "}
-            <div className="h-1/3 border-2 rounded-xl flex flex-row justify-between items-center p-6">
-              <div className="w-1/5 flex space-x-4">
-                <div>
-                  <Image alt="" src={busMonitor} />
-                </div>
-                <div className="bg-white rounded-xl px-12 py-2 my-auto">
-                  <p className="text-black text-7xl">2</p>
-                </div>
-              </div>
-              <div className="w-4/5 flex justify-end">
-                <p className="text-white text-8xl font-semibold">10 Menit </p>
-              </div>
-            </div>{" "}
-            <div className="h-1/3 border-2 rounded-xl flex flex-row justify-between items-center p-6">
-              <div className="w-1/5 flex space-x-4">
-                <div>
-                  <Image alt="" src={busMonitor} />
-                </div>
-                <div className="bg-white rounded-xl px-12 py-2 my-auto">
-                  <p className="text-black text-7xl">2</p>
-                </div>
-              </div>
-              <div className="w-4/5 flex justify-end">
-                <p className="text-white text-8xl font-semibold">10 Menit </p>
-              </div>
-            </div>
+            {mockData.map((d) => {
+              return <MonitorEstimate timeLeft={d.timeLeft} num={d.number} />;
+            })}
           </div>
         </>
       ) : i === 2 ? (
         <>
-          {" "}
           <div className="bg-black h-screen w-screen flex flex-col space-y-6 p-8 text-white">
-            <div className="h-1/3 border-2 rounded-xl flex flex-row justify-between items-center p-6">
-              <div className="w-1/5 flex space-x-4">
-                <div>
-                  <Image alt="" src={busMonitor} />
-                </div>
-                <div className="bg-white rounded-xl px-12 py-2 my-auto">
-                  <p className="text-black text-7xl">2</p>
-                </div>
-              </div>
-              <div className="w-4/5 flex justify-end">
-                <div className="bg-red-primary px-16 py-4 rounded-2xl">
-                  <p className="text-white text-8xl">Rute lurus </p>
-                </div>
-              </div>
-            </div>{" "}
-            <div className="h-1/3 border-2 rounded-xl flex flex-row justify-between items-center p-6">
-              <div className="w-1/5 flex space-x-4">
-                <div>
-                  <Image alt="" src={busMonitor} />
-                </div>
-                <div className="bg-white rounded-xl px-12 py-2 my-auto">
-                  <p className="text-black text-7xl">2</p>
-                </div>
-              </div>
-              <div className="w-4/5 flex justify-end">
-                <div className="bg-red-primary px-16 py-4 rounded-2xl">
-                  <p className="text-white text-8xl">Rute lurus </p>
-                </div>
-              </div>
-            </div>{" "}
-            <div className="h-1/3 border-2 rounded-xl flex flex-row justify-between items-center p-6">
-              <div className="w-1/5 flex space-x-4">
-                <div>
-                  <Image alt="" src={busMonitor} />
-                </div>
-                <div className="bg-white rounded-xl px-12 py-2 my-auto">
-                  <p className="text-black text-7xl">2</p>
-                </div>
-              </div>
-              <div className="w-4/5 flex justify-end">
-                <div className="bg-red-primary px-16 py-4 rounded-2xl">
-                  <p className="text-white text-8xl">Rute lurus </p>
-                </div>
-              </div>
-            </div>
+            {mockData.map((d) => {
+              return <MonitorRoute route={d.route} num={d.number} />;
+            })}
           </div>
         </>
       ) : i === 3 ? (
         <>
           {" "}
           <div className="bg-black h-screen w-screen flex flex-col space-y-6 p-8 text-white">
-            <div className="h-1/3 border-2 rounded-xl flex flex-row justify-between items-center p-6">
-              <div className="w-1/5 flex space-x-4">
-                <div>
-                  <Image alt="" src={busMonitor} />
-                </div>
-                <div className="bg-white rounded-xl px-12 py-2 my-auto">
-                  <p className="text-black text-7xl">2</p>
-                </div>
-              </div>
-              <div className="w-4/5 flex justify-end">
-                <div className="flex space-x-4 2xl:space-x-8 w-2/5 2xl:w-1/3">
-                  <Image alt="" src={sepi} />
-                  <p className="text-white text-8xl text-[#3EBC70]">Sepi </p>
-                </div>
-              </div>
-            </div>{" "}
-            <div className="h-1/3 border-2 rounded-xl flex flex-row justify-between items-center p-6">
-              <div className="w-1/5 flex space-x-4">
-                <div>
-                  <Image alt="" src={busMonitor} />
-                </div>
-                <div className="bg-white rounded-xl px-12 py-2 my-auto">
-                  <p className="text-black text-7xl">2</p>
-                </div>
-              </div>
-              <div className="w-4/5 flex justify-end">
-                <div className="flex space-x-4 2xl:space-x-8 w-2/5 2xl:w-1/3">
-                  <Image alt="" src={penuh} />
-                  <p className="text-white text-8xl text-red-primary">
-                    Penuh{" "}
-                  </p>
-                </div>
-              </div>
-            </div>{" "}
-            <div className="h-1/3 border-2 rounded-xl flex flex-row justify-between items-center p-6">
-              <div className="w-1/5 flex space-x-4">
-                <div>
-                  <Image alt="" src={busMonitor} />
-                </div>
-                <div className="bg-white rounded-xl px-12 py-2 my-auto">
-                  <p className="text-black text-7xl">2</p>
-                </div>
-              </div>
-              <div className="w-4/5 flex justify-end">
-                <div className="flex space-x-4 2xl:space-x-8 w-2/5 2xl:w-1/3">
-                  <Image alt="" src={penuh} />
-                  <p className="text-white text-8xl text-red-primary">
-                    Penuh{" "}
-                  </p>
-                </div>
-              </div>
+            <div className="bg-black h-screen w-screen flex flex-col space-y-6 p-8 text-white">
+              {mockData.map((d) => {
+                return <MonitorStatus status={d.status} num={d.number} />;
+              })}
             </div>
           </div>
         </>
